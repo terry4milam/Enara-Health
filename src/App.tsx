3e7b80bb00892f00c9c7a2e4a4f24625b6567c78
Terry Milam <toptowermilan@gmail.com>
2022-02-25 15:38:42 -0300

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,57 @@
 import { useState, useEffect } from 'react';
-import Tile from './components/LetterBox';
-import Word from './components/Word';
-import ClearWord from './components/ClearWord';
+import Tile from './components/Tile';
+import WordCreator from './components/WordCreator';
+import Restart from './components/Restart';
 import Layout from './components/Layout';
-import Grid from './components/Grid';
+import Grid from './components/styled/Grid';
 import useFetch from './hooks/useFetch';
 
-type IBoard = string[]
+interface IBoard {
+  board: string[]
+}
 
 function App() {
-  const { data: dataBoard, isFetching } = useFetch<IBoard>('./src/assets/demo.json')
-  const [letters, setLetters] = useState<IBoard | null>([])
+  const { data: boardA, isFetching: fetchingA } = useFetch<IBoard>('./src/assets/demo.json')
+  const { data: boardB, isFetching: fetchingB } = useFetch<IBoard>('./src/assets/demo.json')
+  const [letters, setLetters] = useState<string[] | null>(null)
+  const [word, setWord] = useState<string>('')
+  const [selectedWords, setSelectedWords] = useState<string[]>([])
+  const [isValidWord, setIsValidWord] = useState<boolean | null>(null)
   useEffect(() => {
-    if (!isFetching) {
-      setLetters(dataBoard)
+    console.log({ boardA, boardB, fetchingA, word })
+    if (!fetchingA) {
+      setLetters(boardA!.board)
+    }
+    if (word.length > 0) {
+      setIsValidWord(['lala', 'lilo', 'abc'].includes(word.toLowerCase()))
     }
   })
-
+  if (fetchingA) {
+    return <span>prelaod</span>
+  }
+  const restart = () => {
+    setWord('')
+    setIsValidWord(null)
+  }
+  const setWordArr = (w: string) => {
+    setWord(word.concat('', w))
+    setSelectedWords([...selectedWords, w])
+    console.log({ word, selectedWords })
+  }
   return (<Layout>
-    <ClearWord />
+    <Restart cleanAll={restart} />
     <Grid>
       {
         letters?.map((letter: string) =>
-          <Tile key={letter} letter={letter} />
+          <Tile
+            key={letter}
+            letter={letter}
+            valid={isValidWord}
+            action={letter => setWordArr(letter)} />
         )
       }
     </Grid>
-    <Word words={['a', 'b']} />
+    <WordCreator word={word} valid={isValidWord} />
   </Layout>)
 }
 
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,5 @@
-import styled from "styled-components";
+import Main from './styled/Main';
 
-const Main = styled.main`
-  display: grid;
-  grid-template-columns: repeat(2, 410px);
-  grid-template-rows: repeat(10, .5fr);
-  padding: 2em;
-  max-width: 820px;
-  margin: 0 auto;
-  @media (max-width: 768px) {
-    grid-template-columns: 1fr;
-    height: 90vh;
-  }
-`
 type ILayout = {
   children: JSX.Element[]
 }
