574fddbed5518c969f47b73af5204f2ed49ee3c2
Terry Milam <toptowermilan@gmail.com>
2022-02-24 21:02:43 -0300

    feat(UI): fix border gradient

--- src/components/LetterBox.tsx
+++ src/components/LetterBox.tsx
@@ -4,35 +4,33 @@ import styled from 'styled-components';
 const Button = styled.button`
   width: 62px;
   height: 62px;
-  @media(min-width: 768px){
+  @media(min-width: 769px){
     width: 86px;
     height: 86px;
   }
+  border: none;
+  border-radius: 8px;
+  position: relative;
+  background: linear-gradient(to bottom, #f5515f, #a0051c);
+  &.choosen, &.available{
+    background: transparent;
+  }
+  overflow: hidden;
+`
+const TileSpan = styled.span`
   font-family: 'tahoma-small-cap-bold', sans-serif;
   font-size: 1.6rem;
   text-shadow: -1px -1px 2px rgba(0, 0, 0, 0.42);
   text-transform: uppercase;
   color: white;
   background: linear-gradient(to bottom, #FAD15C, #F76E1E);
-  background-clip: padding-box;
-  border: solid 2px transparent;
-  border-radius: 8px;
-  line-height: 1;
-  margin: auto;
-  position: relative;
-  box-sizing: border-box;
-  &:before {
-    content: '';
-    position: absolute;
-    top: 0; right:0; bottom:0; left:0;
-    z-index: -1;
-    margin: -2px;
-    border-radius: inherit;
-    background: linear-gradient(to bottom, red, orange);
-  }
-  &.choosen, &.available{
-    border: none;
-  }
+  text-transform: uppercase;
+  position: absolute;
+  border-radius: 6px;
+  top:3px; left:3px; bottom:3px; right:3px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   &.choosen {
     background: linear-gradient(to bottom, #f4505f, #9f041b);
   }
@@ -40,7 +38,6 @@ const Button = styled.button`
     background: linear-gradient(to bottom, #b3ec50, #439422);
   }
 `
-
 interface ITile {
   letter: string
 }
@@ -50,7 +47,9 @@ const Tile = (props: ITile): JSX.Element => {
     setClassTile('available')
   }
   return (<Button className={classTile} onClick={chooseTile}>
-    {props.letter}
+    <TileSpan className={classTile}>
+      {props.letter}
+    </TileSpan>
   </Button>)
 }
 export default Tile;
