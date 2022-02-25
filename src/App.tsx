e9b74066b982a763b1af40cc9b5fa6ee73cda19d
Terry Milam <toptowermilan@gmail.com>
2022-02-25 00:35:20 -0300

    feat(hooks): update hook function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Tile from './components/LetterBox';
 import Word from './components/Word';
 import ClearWord from './components/ClearWord';
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios'
 
-const useFetch = (url: string) => {
+export default function useFetch<T = unknown>(url: string) {
   const [data, setData] = useState<T | null>(null)
   const [isFetching, setIsFetching] = useState(true)
   const [error, setError] = useState(false)
   useEffect(() => {
     axios.get(url)
-      .then(data => {
-        setData(data)
-      }).catch(() => setError(true)).finally(() => setIsFetching(false))
+      .then(response => {
+        setData(response.data)
+      })
+      .catch(() => {
+        setError(true)
+      })
+      .finally(() => {
+        setIsFetching(false)
+      })
   }, [])
   return { data, isFetching, error }
 }
 
-export default useFetch;
